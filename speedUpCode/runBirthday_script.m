04369b56d9f0696c38d4cee4eee89f14fbed4313
fraptic <57094310+fraptic@users.noreply.github.com>
2020-06-16 16:58:54 +0200

    Add files via upload

---
+++ speedUpCode/runBirthday_script.m
@@ -0,0 +1,43 @@
+% Runs a Monte Carlo simulation using the Birthday Paradox
+
+numTrials = 1e5;
+groupSize = 30;
+
+matches = false(1, numTrials);
+for trial = 1:numTrials
+    % Run a simulation for a group
+    matches(trial) = birthday(groupSize);
+end
+
+% Probability is the sum of matches divided by number of trials
+count = 0;
+for trial = 1:numTrials
+    count = count + matches(trial);
+end
+prob = count / numTrials;
+
+
+function match = birthday(groupSize)
+% BIRTHDAY Simulates a single trial of the Birthday Paradox
+
+% Match is false until a birthday match is found
+match = false;
+
+% Initialize list of taken birthdays
+bdaylist = zeros(1, groupSize);
+
+for person = 1:groupSize
+    % Randomly select a birthdate for the individual (ignore leap years)
+    birthdate = randi(365);
+
+    % Check if someone else in the group shares the same birthday
+    if any(birthdate == bdaylist)
+        % A match is found, return from the function
+        match = true;
+        return;
+    end
+
+    % Add the birthdate to the list for the group
+    bdaylist(person) = birthdate;
+end
+end
